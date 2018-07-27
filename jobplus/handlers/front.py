da40bc31496b5ec715cb5078d5af0d47b634d4a2
zxspython <4454230.hi@163.com>
2018-07-28 01:18:58 +0800

    edit front.py and forms.py

--- jobplus/handlers/front.py
+++ jobplus/handlers/front.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 # -*- coding: <encoding name> -*-
 
-from flask import Blueprint,render_template
+from flask import Blueprint,render_template,flash,redirect,url_for
+from jobplus.forms import RegisterForm
+from jobplus.models import db,User
 
 front = Blueprint('front',__name__,url_prefix='/')
 
@@ -10,12 +12,17 @@ def index():
     return render_template('index.html')
 
 
-@front.route('/companyregister',methods=['GET','POST'])  #注册路由，提供get,post方法
+@front.route('/companyregister',methods=['GET','POST'])  
+#注册路由，提供get,post方法
 def companyregister():
-      form =RegisterForm()  #引用flask-WTF,基类FlaskForm生成的对象。实现，pychon创建html的form。
-      form.name.label = '企业用户' #改变form.name.label 属性值（个人用户为企业用户）
-      if form.validate_on_submit():  # validate_on_submit来自于，flask-WTF,提供的FlaskForm的方法中。
-          company_user = form.create_user() #将form从用户得到的数据，传给数据库。返回一个User对象。
+      form = RegisterForm()  
+      #引用flask-WTF,基类FlaskForm生成的对象。实现，pychon创建html的form。
+      form.name.label = '企业用户' 
+      #改变form.name.label 属性值（个人用户为企业用户）
+      if form.validate_on_submit():  
+          # validate_on_submit来自于，flask-WTF,提供的FlaskForm的方法中。
+          company_user = form.create_user() 
+          #将form从用户得到的数据，传给数据库。返回一个User对象。
           company_user.role = User.ROLE_COMPANY #改变User对象role
           db.session.add(company_user) #更新数据
           dv.session.commit
