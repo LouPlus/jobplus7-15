57ae963e1313a0e4f8e64be34c9bd6ecc8b1da9f
zxspython <37393506+zxspython@users.noreply.github.com>
2018-07-30 08:39:14 +0800

    this closes #37 and closes #38 (#39)
    * edit front.py
    
    * edit froms.py and front.py create companyregister.html
    
    * edit form.py
    
    * edit front.py and forms.py
    
    * touch macros.html
    
    * edit front.py
    
    * Update front.py
    
    * Update forms.py
    
    * edit macros.html
    
    * edit userregister
    
    * edit userregister
    
    * edit companyregister.html

--- jobplus/forms.py
+++ jobplus/forms.py
@@ -1,3 +1,32 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
+from flash-wtf import FlashForm
+from wtforms import StringField,PasswordField,SubmitField 
+from wtforms.validators import Required,Length,EqualTo,Email
+from jobplus.models import db,User
+
+class RegisterForm():
+    username = StringField('用户名',validators=[Required(),Length(3,24)])
+    email = StringField('邮件',validators=[Required(),Email()])
+    password = PasswordField('密码',validators=[Required(),Length(6,24)])
+    repeat_password = PasswordField('重复密码',validators=[Required(),EqualTo('password')])
+    submit = SubmitField()
+
+    def validate_username(self,field):
+        if User.query.filter_by(name=field.data).first():
+            raise ValidationError('名字已存在')
+
+
+    def validate_email(self,field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError('邮箱已存在')
+
+                           
+    def create_user(self):   #在数据中创建User
+        user = User(name=self.username.data,
+                    email=self.email.data,
+                    password=self.password.data) #将form 相应数据传入
+        db.session.add(user)
+        db.session.commit()
+        return user
